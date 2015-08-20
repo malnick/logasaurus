98b39254f4f7b1f4731ecab82cfff478b773f18f
malnick <malnick@gmail..com>
2015-08-19 19:51:43 -0700

    flags

---
+++ logit.go
@@ -0,0 +1,25 @@
+package main
+
+import (
+	"log"
+	"flag"
+)
+
+type Flags struct {
+	Config bool,
+	Define string,
+	Elastic_uri string,
+	Interval int,
+	Index string,
+	Port int,
+	Verbose bool,
+}
+
+func (f Flags) options() {
+
+}
+
+func main() {
+
+
+}
