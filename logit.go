17faaa73eefaa64eb8df8232a1c5bd69fa25bf82
malnick <malnick@gmail..com>
2015-08-19 20:58:42 -0700

    glags

--- logit.go
+++ logit.go
@@ -6,23 +6,24 @@ import (
 )
 
 type Flags struct {
-	config bool,
-	define string,
-	elastic_uri string,
-	interval int,
-	index string,
-	port int,
-	verbose bool,
+	config bool
+	define string
+	elastic_uri string
+	interval int
+	index string
+	port int
+	verbose bool
 }
 
-func options() (f Flags, err error)  {
+func options() Flags {
 	f.Config := flag.Bool("config", false, "The path to the logit.yaml. Default: ~/.logit/config.yaml (osx) and /etc/logit/config.yaml (*nix).")
-
-	return f
+	log.Println("Configuration:")
+	log.Println(f)
+	return f, nil
 }
 
 func main() {
 	// Get cli flags
 	flags := options()
-
+	log.Println(flags)
 }
