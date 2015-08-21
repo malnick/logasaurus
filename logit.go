6aa9a2dcd29b6e29975c16f8fe790951bf309ee3
malnick <malnick@gmail..com>
2015-08-21 12:20:01 -0700

    logit.go

--- logit.go
+++ logit.go
@@ -1,29 +1,34 @@
 package main
 
 import (
-	"log"
 	"flag"
+	"log"
 )
 
 type Flags struct {
-	config bool
-	define string
-	elastic_uri string
-	interval int
-	index string
-	port int
-	verbose bool
+	config      *bool
+	define      *string
+	elastic_uri *string
+	interval    *int
+	index       *string
+	port        *int
+	verbose     *bool
 }
 
-func options() Flags {
-	f.Config := flag.Bool("config", false, "The path to the logit.yaml. Default: ~/.logit/config.yaml (osx) and /etc/logit/config.yaml (*nix).")
-	log.Println("Configuration:")
-	log.Println(f)
-	return f, nil
+func options() (f Flags) {
+	config := flag.Bool("config", false, "The path to the logit.yaml. Default: ~/.logit/config.yaml (osx) and /etc/logit/config.yaml (*nix).")
+	define := flag.String("define", "", "A one-time defined service. Must be valid ES query.")
+
+	f.config = config
+	f.define = define
+	return f
 }
 
 func main() {
 	// Get cli flags
 	flags := options()
-	log.Println(flags)
+	log.Println("Configuration:\n")
+	for k, v := range flags {
+		log.Println(k, v)
+	}
 }
