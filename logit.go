b69956c7f99b1a37eed3d72c1627aac7dcd6b836
malnick <malnick@gmail..com>
2015-08-22 08:08:42 -0700

    logit.go

--- logit.go
+++ logit.go
@@ -5,23 +5,27 @@ import (
 	"log"
 )
 
-type Flags struct {
-	config      *bool
-	define      *string
-	elastic_uri *string
-	interval    *int
-	index       *string
-	port        *int
-	verbose     *bool
-}
+//	config      *bool
+//	define      *string
+//	elastic_uri *string
+//	interval    *int
+//	index       *string
+//	port        *int
+//	verbose     *bool
+//}
 
-func options() (f Flags) {
-	config := flag.Bool("config", false, "The path to the logit.yaml. Default: ~/.logit/config.yaml (osx) and /etc/logit/config.yaml (*nix).")
-	define := flag.String("define", "", "A one-time defined service. Must be valid ES query.")
+var config_file map[string]string
 
-	f.config = config
-	f.define = define
-	return f
+func options() (o map[string]interface{}) {
+
+	config := flag.Bool("c", false, "The path to the logit.yaml. Default: ~/.logit/config.yaml (osx) and /etc/logit/config.yaml (*nix).")
+	o["config"] = config
+
+	//	o["define"] = flag.String("d", "", "A one-time defined service. Must be valid ES query.")
+	//	o["elastic_url"] = flag.String("e", "localhost:9300", "Elastic search URL. Default: localhost:9300")
+	//	o["interval"] = flag.Int("i", 1, "Query interval in seconds. Default: 1")
+
+	return o
 }
 
 func main() {
