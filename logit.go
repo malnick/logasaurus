d839f96532d023d3fac8403dea50a1aaecb94128
malnick <malnick@gmail..com>
2015-08-27 12:24:24 -0700

    logit.go

--- logit.go
+++ logit.go
@@ -2,16 +2,27 @@ package main
 
 import (
 	"flag"
+	"gopkg.in/yaml.v2"
+	"io/ioutil"
 	"log"
 )
 
 // Define flag overrides
-var config_path = flag.String("c", "~/.logit/config.yaml", "The path to the logit.yaml. Default: ~/.logit/config.yaml (osx) and /etc/logit/config.yaml (*nix).")
+var config_path = flag.String("c", "./config.yaml", "The path to the logit.yaml. Default: ~/.logit/config.yaml (osx) and /etc/logit/config.yaml (*nix).")
 var define_service = flag.String("d", "", "A one-time defined service. Must be valid ES query.")
 var elastic_url = flag.String("e", "localhost:9300", "Elastic search URL. Default: localhost:9300")
 var sync_interval = flag.Int("i", 1, "Query interval in seconds. Default: 1")
 
-func options() (o map[string]interface{}, err error) {
+func options(config_path string) (o map[string]interface{}, err error) {
+	config_file, err := ioutil.ReadFile(config_path)
+	if err != nil {
+		return nil, err
+	}
+
+	err = yaml.Unmarshal(config_file, &o)
+	if err != nil {
+		return nil, err
+	}
 
 	return o, nil
 }
@@ -19,5 +30,11 @@ func options() (o map[string]interface{}, err error) {
 func main() {
 	// Get cli flags
 	flag.Parse()
-	log.Println("Define: ", *define_service)
+	config, err := options(*config_path)
+	if err != nil {
+		log.Println(err)
+	}
+	for k, v := range config {
+		log.Println(k, v)
+	}
 }
