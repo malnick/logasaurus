3ae89d8b1f7561e586e7f1a684d7a9214abff569
malnick <malnick@gmail..com>
2015-08-27 16:22:18 -0700

    using config struct

--- logit.go
+++ logit.go
@@ -10,6 +10,15 @@ import (
 	//	"net/http"
 )
 
+// Config struct
+type Config struct {
+	Define              map[string]string `yaml:"define"`
+	Sync_interval       int               `yaml:"sync_interval"`
+	Elasticsearch_url   string            `yaml:"elasticsearch_url"`
+	Elasticsearch_port  string            `yaml:"elasticsearch_port"`
+	Elasticsearch_index string            `yaml:"elasticsearch_index"`
+}
+
 // Define flag overrides
 var config_path = flag.String("c", "./config.yaml", "The path to the logit.yaml. Default: ~/.logit/config.yaml (osx) and /etc/logit/config.yaml (*nix).")
 var define_service = flag.String("d", "", "A one-time defined service. Must be valid ES query.")
@@ -20,28 +29,28 @@ var elastic_index = flag.String("in", "", "Elastic Search index. Default: logsta
 var verbose = flag.Bool("v", false, "Verbosity. Default: false")
 var service = flag.String("s", "", "Query already defined service in config.yaml.")
 
-func options(config_path string) (o map[string]interface{}, err error) {
+func options(config_path string) (o Config, err error) {
 	config_file, err := ioutil.ReadFile(config_path)
 	if err != nil {
-		return nil, err
+		return o, err
 	}
 	// Unmarshal the config to a map
 	err = yaml.Unmarshal(config_file, &o)
 	if err != nil {
-		return nil, err
+		return o, err
 	}
 	// Override a million things
 	if len(*elastic_url) > 1 {
-		o["elasticsearch_url"] = *elastic_url
+		o.Elasticsearch_url = *elastic_url
 	}
 	if *sync_interval > 0 {
-		o["sync_interval"] = *sync_interval
+		o.Sync_interval = *sync_interval
 	}
 	if len(*elastic_port) > 0 {
-		o["elasticsearch_port"] = *elastic_port
+		o.Elasticsearch_port = *elastic_port
 	}
 	if len(*elastic_index) > 0 {
-		o["elasticsearch_index"] = *elastic_index
+		o.Elasticsearch_index = *elastic_index
 	}
 
 	return o, nil
@@ -96,12 +105,16 @@ func main() {
 	}
 
 	log.Debug("Configuration:")
-	for k, v := range config {
-		log.Debug(k, v)
-	}
+	log.Debug(
+		"Defines: ", config.Define, "\n",
+		"Sync: ", config.Sync_interval, "\n",
+		"ES URL: ", config.Elasticsearch_url, "\n",
+		"ES Port: ", config.Elasticsearch_port, "\n",
+		"ES Index: ", config.Elasticsearch_index)
+
 	// Query string: from CLI or config.yaml?
 	// Assert map string string type for defines sub map in config
-	defines := config["define"].(map[string]string)
+	defines := config.Define
 	svc_query := lookup(defines)
 	log.Debug(svc_query)
 
