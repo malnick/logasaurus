30063d09f8b406aa7f16ef039136396f03be9711
malnick <malnick@gmail..com>
2015-08-27 12:31:32 -0700

    override uri

--- logit.go
+++ logit.go
@@ -10,7 +10,7 @@ import (
 // Define flag overrides
 var config_path = flag.String("c", "./config.yaml", "The path to the logit.yaml. Default: ~/.logit/config.yaml (osx) and /etc/logit/config.yaml (*nix).")
 var define_service = flag.String("d", "", "A one-time defined service. Must be valid ES query.")
-var elastic_url = flag.String("e", "localhost:9300", "Elastic search URL. Default: localhost:9300")
+var elastic_url = flag.String("e", "", "Elastic search URL. Default: localhost:9300")
 var sync_interval = flag.Int("i", 1, "Query interval in seconds. Default: 1")
 
 func options(config_path string) (o map[string]interface{}, err error) {
@@ -24,6 +24,9 @@ func options(config_path string) (o map[string]interface{}, err error) {
 		return nil, err
 	}
 
+	if len(*elastic_url) > 1 {
+		o["elasticsearch_url"] = *elastic_url
+	}
 	return o, nil
 }
 
